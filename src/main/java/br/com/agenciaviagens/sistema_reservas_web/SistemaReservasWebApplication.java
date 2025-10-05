32dd932a156ddccc99e5c691663e7a724e428c25
Lucas Neves <lucasneves4004@gmail.com>
2025-10-05 20:09:58 -0300

    Spring

---
+++ src/main/java/br/com/agenciaviagens/sistema_reservas_web/SistemaReservasWebApplication.java
@@ -0,0 +1,13 @@
+package br.com.agenciaviagens.sistema_reservas_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaReservasWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaReservasWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/agenciaviagens/sistema_reservas_web/SistemaReservasWebApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.agenciaviagens.sistema_reservas_web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaReservasWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
