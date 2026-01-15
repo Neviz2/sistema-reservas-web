2844bf6052fd447868c8be98d4d3391b47b884db
Lucas Neves <lucasneves4004@gmail.com>
2026-01-14 21:43:37 -0300

    Remove empty comment from ReservaRepository
    Removed empty comment indicating that Spring handles functionality.

--- src/main/java/br/com/agenciaviagens/sistema_reservas_web/repository/ReservaRepository.java
+++ src/main/java/br/com/agenciaviagens/sistema_reservas_web/repository/ReservaRepository.java
@@ -4,5 +4,4 @@ import br.com.agenciaviagens.sistema_reservas_web.model.Reserva;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface ReservaRepository extends MongoRepository<Reserva, String> {
-    // Vazio! O Spring faz a mágica.
 }
